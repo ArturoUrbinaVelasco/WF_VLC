997e0f974a850910f299d18c8bd7c819024acba0
Arturo Urbina Velasco <48217956+ArturoUrbinaVelasco@users.noreply.github.com>
2025-04-10 16:58:58 -0600

    Add files via upload

---
+++ WF_VLC/WF_VLC/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WF_VLC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tsbtn_play = new System.Windows.Forms.ToolStripButton();
+            this.tsbtn_stop = new System.Windows.Forms.ToolStripButton();
+            this.tsbtn_find_capturadora = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.ts_cmbDevices = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tsbtn_play
+            // 
+            this.tsbtn_play.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtn_play.Image = ((System.Drawing.Image)(resources.GetObject("tsbtn_play.Image")));
+            this.tsbtn_play.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtn_play.Name = "tsbtn_play";
+            this.tsbtn_play.Size = new System.Drawing.Size(23, 22);
+            this.tsbtn_play.Text = "Play";
+            this.tsbtn_play.Click += new System.EventHandler(this.tsbtn_play_Click);
+            // 
+            // tsbtn_stop
+            // 
+            this.tsbtn_stop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtn_stop.Image = ((System.Drawing.Image)(resources.GetObject("tsbtn_stop.Image")));
+            this.tsbtn_stop.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtn_stop.Name = "tsbtn_stop";
+            this.tsbtn_stop.Size = new System.Drawing.Size(23, 22);
+            this.tsbtn_stop.Text = "Stop";
+            this.tsbtn_stop.Click += new System.EventHandler(this.tsbtn_stop_Click);
+            // 
+            // tsbtn_find_capturadora
+            // 
+            this.tsbtn_find_capturadora.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtn_find_capturadora.Image = ((System.Drawing.Image)(resources.GetObject("tsbtn_find_capturadora.Image")));
+            this.tsbtn_find_capturadora.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtn_find_capturadora.Name = "tsbtn_find_capturadora";
+            this.tsbtn_find_capturadora.Size = new System.Drawing.Size(23, 22);
+            this.tsbtn_find_capturadora.Text = "find_capturadora";
+            this.tsbtn_find_capturadora.Click += new System.EventHandler(this.tsbtn_find_capturadora_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbtn_play,
+            this.tsbtn_stop,
+            this.tsbtn_find_capturadora,
+            this.ts_cmbDevices});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // ts_cmbDevices
+            // 
+            this.ts_cmbDevices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ts_cmbDevices.Name = "ts_cmbDevices";
+            this.ts_cmbDevices.Size = new System.Drawing.Size(121, 25);
+            this.ts_cmbDevices.Sorted = true;
+            this.ts_cmbDevices.DropDownClosed += new System.EventHandler(this.ts_cmbDevices_DropDownClosed);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reproductor de Video";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripButton tsbtn_play;
+        private System.Windows.Forms.ToolStripButton tsbtn_stop;
+        private System.Windows.Forms.ToolStripButton tsbtn_find_capturadora;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox ts_cmbDevices;
+    }
+}
+
---
+++ WF_VLC/WF_VLC/Form1.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Vlc.DotNet.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace WF_VLC
+{
+    public partial class Form1 : Form
+    {
+        private VlcControl vlcControl;
+        string vlcPath = "";
+        private string lastSelectedDevice = null;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeVlcControl();
+        }
+
+        private void InitializeVlcControl()
+        {
+            vlcControl = new VlcControl();
+            vlcControl.Dock = DockStyle.Fill;
+            this.Controls.Add(vlcControl);
+
+            var vlcLibDirectory = new DirectoryInfo(@"C:\Program Files\VideoLAN\VLC");
+
+            // Configurar opciones de reproducción
+            var options = new string[]
+            {
+                ":network-caching=300", // Aumentar el buffer de red
+                ":file-caching=300",    // Aumentar el buffer de archivo
+                ":no-video-title-show", // Ocultar el título del video
+                ":no-audio",            // Desactivar audio si no es necesario
+                ":rtsp-tcp",              // Usar TCP para transmisiones RTSP
+
+                ":avcodec-hw=dxva2", // Habilitar aceleración por hardware con DXVA2
+                ":network-caching=300",
+                ":file-caching=300",
+
+                ":verbose=2", // Nivel de verbosidad del log
+                ":logfile=vlc_log.txt" // Guardar logs en un archivo
+            };
+
+            vlcControl.BeginInit();
+            vlcControl.VlcLibDirectory = vlcLibDirectory;
+            vlcControl.VlcMediaplayerOptions = options; // Aplicar las opciones
+            vlcControl.EndInit();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void tsbtn_play_Click(object sender, EventArgs e)
+        {
+            PlayVideo();
+        }
+        private void PlayVideo()
+        {
+            // Reproducir un archivo de video
+            vlcControl.SetMedia(new Uri(@"C:\Videos\full_REPRO_00.mp4"));
+            vlcControl.Play();
+        }
+
+        private void tsbtn_stop_Click(object sender, EventArgs e)
+        {
+            StopVideo();
+        }
+        private void StopVideo()
+        {
+            // Detener la reproducción
+            vlcControl.Stop();
+        }
+
+        private void tsbtn_find_capturadora_Click(object sender, EventArgs e)
+        {
+            // Valida VLC
+            vlcPath = GetVLCPath();
+
+            if (!string.IsNullOrEmpty(vlcPath))
+            {
+                LoadVideoDevices();
+            }
+            else
+            {
+                MessageBox.Show("VLC no está instalado.", "VLC No Instalado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private string GetVLCPath()
+        {
+            string registryPath = @"SOFTWARE\VideoLAN\VLC";
+
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(registryPath))
+            {
+                if (key != null)
+                {
+                    object installDir = key.GetValue("InstallDir");
+                    if (installDir != null)
+                    {
+                        return installDir.ToString();
+                    }
+                }
+            }
+
+            return null;
+        }
+        private void LoadVideoDevices()
+        {
+            try
+            {
+                // Ejecutar el comando FFmpeg para obtener los dispositivos de video
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/C ffmpeg -list_devices true -f dshow -i dummy 2>&1",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = new Process { StartInfo = psi })
+                {
+                    process.Start();
+                    string output = process.StandardOutput.ReadToEnd();
+                    process.WaitForExit();
+
+                    // Expresión regular para extraer nombres de dispositivos de video
+                    Regex regex = new Regex(@"""(.*?)"" \(video\)");
+                    MatchCollection matches = regex.Matches(output);
+
+                    ts_cmbDevices.Items.Clear();
+                    foreach (Match match in matches)
+                    {
+                        ts_cmbDevices.Items.Add(match.Groups[1].Value);
+                    }
+
+                    if (ts_cmbDevices.Items.Count > 0)
+                        ts_cmbDevices.SelectedIndex = 0; // Seleccionar el primer elemento por defecto
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener dispositivos de video: " + ex.Message);
+            }
+        }
+        private void ts_cmbDevices_DropDownClosed(object sender, EventArgs e)
+        {
+            if (ts_cmbDevices.Items.Count > 0 && ts_cmbDevices.SelectedItem != null)
+            {
+                string selectedDevice = ts_cmbDevices.SelectedItem.ToString();
+
+                // Solo ejecutar si el dispositivo seleccionado es diferente al anterior
+                if (selectedDevice != lastSelectedDevice)
+                {
+                    lastSelectedDevice = selectedDevice; // Actualizar el último seleccionado
+                    PlayCaptureDevice(selectedDevice);
+                }
+            }
+        }
+        private void PlayCaptureDevice(string deviceName)
+        {
+            //Reproducir la señal de un dispositivo de captura
+            string mediaOptions = $":dshow-vdev={deviceName}";
+
+            vlcControl.SetMedia(new Uri("dshow://"), mediaOptions);
+            vlcControl.Play();
+        }
+
+    }
+}
